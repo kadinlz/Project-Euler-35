c9cbf742b76ec8e1205e991a97eb37bde020db00
Kadin Zhang <cykadin@gmail.com>
2019-04-03 09:49:20 -0400

    add ui boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			num: 100,
+		};
+	}
 	render() {
 		return (
 			<div className="App">
 				<div className="inputWrapper">
-					<input className="input" autofocus="true" type="number" value="10000000" />
+					<p className="header">Project Euler Problem 35</p>
+					<p className="description">How many circular primes are below:</p>
+					<input
+						className="input"
+						autofocus="true"
+						type="number"
+						value={this.state.num}
+						onChange={this.handleChange}
+					/>
 				</div>
 			</div>
 		);
