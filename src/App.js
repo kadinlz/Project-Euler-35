8d4be3ef215b39ca9e43ed7e8e5c855d314ccf37
Kadin Zhang <cykadin@gmail.com>
2019-04-04 17:22:50 -0400

    bump font; create two-way data binding input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { FaInfoCircle } from 'react-icons/fa';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			num: 100,
+			num: 1000000000,
 		};
 	}
+	handleChange = e => {
+		this.setState({
+			num: e.target.value,
+		});
+	};
 	render() {
 		return (
 			<div className="App">
 				<div className="inputWrapper">
-					<p className="header">Project Euler Problem 35</p>
-					<p className="description">How many circular primes are below:</p>
+					<p className="header">
+						Project Euler Problem <span>35</span>
+					</p>
+					<p className="description">
+						How many circular primes
+						<a className="info" target=" " href="https://projecteuler.net/problem=35">
+							<FaInfoCircle />
+						</a>
+						are below:
+					</p>
 					<input
 						className="input"
 						autofocus="true"
